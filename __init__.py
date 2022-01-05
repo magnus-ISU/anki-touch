e9661d206873530b6b273c11454db8b70f9e8400
magnus <magnus@iastate.edu>
2022-01-05 14:42:31 -0600

    Accurately get all the signals we want, but just do debug prints for now

--- __init__.py
+++ __init__.py
@@ -17,116 +17,94 @@ from aqt.clayout import CardLayout
 from aqt.qt import Qt
 from aqt.reviewer import Reviewer
 
-from aqt.utils import tooltip
+# Globals
+inQuestion = True
+downX = -1.
+SWIPE_NEEDED_PIXELS = 150
 
 # Responding to clicks
-############################
-
-
 def on_js_message(handled, msg, context):
-    print("on_js_message: " + msg)
+    global downX
+    global inQuestion
+
     if isinstance(context, CardLayout) and (
         msg.startswith("ct_click_") or msg.startswith("ct_dblclick_")
     ):
         # card layout is a modal dialog, so we can't display there
-        tooltip("Can't be used in card layout screen.")
         return handled
 
     if not isinstance(context, Reviewer) and not isinstance(context, PreviewDialog):
         # only function in review and preview screens
         return handled
 
-#    if msg.startswith("ct_click_"):
-#        tag = msg.replace("ct_click_", "")
-#        browser = dialogs.open("Browser", mw)
-#        browser.setFilter('"tag:%s"' % tag)
-#        return True, None
-#    elif msg.startswith("ct_dblclick_"):
-#        tag, deck = msg.replace("ct_dblclick_", "").split("|")
-#        browser = dialogs.open("Browser", mw)
-#        browser.setFilter('"tag:%s" "deck:%s"' % (tag, deck))
-#        browser.setWindowState(
-#            browser.windowState() & ~Qt.WindowMinimized | Qt.WindowActive
-#        )
-#        return True, None
-    if msg.startswith("anki_touch_"):
-        print("on_js_message2")
+    if msg.startswith("anki_touch "):
+        msg = msg.removeprefix("anki_touch ")
+        if msg.startswith("down "):
+            msg = msg.removeprefix("down ")
+            downX = float(msg)
+        elif msg.startswith("up "):
+            msg = msg.removeprefix("up ")
+            diffX = float(msg) - downX
+            if diffX < -SWIPE_NEEDED_PIXELS:
+                if not inQuestion:
+                    print("Failed")
+                else:
+                    print("Show back")
+            elif diffX > SWIPE_NEEDED_PIXELS:
+                if not inQuestion:
+                    print("Successed")
+                else:
+                    print("Show back")
         return True, None
 
     return handled
 
-
 gui_hooks.webview_did_receive_js_message.append(on_js_message)
 
 # Adding CSS/JS to card
-############################
 
-add_to_card = """
-<style>
-  kbd {
-    box-shadow: inset 0 1px 0 0 white;
-    background:
-      gradient(
-        linear,
-        left top,
-        left bottom,
-        color-stop(0.05, #f9f9f9),
-        color-stop(1, #e9e9e9)
-      );
-    background-color: #f9f9f9;
-    border-radius: 4px;
-    border: 1px solid gainsboro;
-    display: inline-block;
-    font-size: 15px;
-    height: 15px;
-    line-height: 15px;
-    padding: 4px 4px;
-    margin: 5px;
-    text-align: center;
-    text-shadow: 1px 1px 0 white;
-    cursor: pointer;
-    cursor: hand;
-  }
-
-  .nightMode kbd { color: black; }
-</style>
+# There are two types of events we listen to: click and touch ones
+# click events are the only ones we care about in question pages, and they show the back side of the card
+# Touchstart/end events and mousedown/up events are handled on the back to accept/reject cards if the x changes sufficiently (hardcoded as 150px)
+swipe_handler = """
 <script type="text/javascript">
-function ct_click(tag) {
-    pycmd("ct_click_" + tag)
+function intercept_down(event) {
+    pycmd("anki_touch down " + event.x)
 }
-function ct_dblclick(tag, deck) {
-    pycmd("ct_dblclick_" + tag + "|" + deck)
+function intercept_up(event) {
+    pycmd("anki_touch up " + event.x)
 }
+function touch_down(event) {
+    touch = event.changedTouches[0];
+    pycmd("anki_touch down " + touch.pageX)
+}
+function touch_up(event) {
+    touch = event.changedTouches[0];
+    pycmd("anki_touch up " + touch.pageX)
+}
+// Do this hack with document.anki_touch because somehow the event listeners persist across questions
+if (!document.anki_touch) {
+    document.addEventListener("mousedown", intercept_down);
+    document.addEventListener("mouseup", intercept_up);
+    document.addEventListener("touchstart", touch_down);
+    document.addEventListener("touchend", touch_up);
+}
+document.anki_touch = true;
 </script>
 """
 
-# Handling {{clickable:Tags}}
-################################
-
-click_handler = """
-<script type="text/javascript">
-function intercept_click(thing) {
-    pycmd("anki_touch_" + thing)
-}
-pycmd("anki_touch_start");
-document.addEventListener("click", intercept_click);
-</script>
-"""
-
-# Inject the javascript that handles clicks
+# Inject the javascript that handles swipes
 def on_card_show(html, card, context):
     print("on_card_show")
-    return html + click_handler
+    return html + swipe_handler
 gui_hooks.card_will_show.append(on_card_show)
 
-inQuestion = True
+# Set the global variable that tracks if we're in question or answer
 def show_question(card):
     global inQuestion
     inQuestion = True
-
 def show_answer(card):
     global inQuestion
     inQuestion = False
-
 gui_hooks.reviewer_did_show_question.append(show_question);
 gui_hooks.reviewer_did_show_answer.append(show_answer);
